ac75889cb2b4b3d1172b060f23052e2cd664c1ea
pe5ha <pavel.naumenko.2000@yandex.ru>
2023-09-20 23:46:21 +0300

    regtion

--- src/UseCases.js
+++ src/UseCases.js
@@ -2,6 +2,68 @@
  *   Bot use cases detecting (User Roles and Current actions)
  */
 function useCases(){
+
+
+  // start
+
+  // if(user.currentAction == UserActions.input_phone){
+  //   if(contents.message.contact) {
+  //     user.setUserPhone(contents.message.contact.phone_number);
+  //     user.setUserCurrentAction(UserActions.input_name);
+  //     botSendMessage(chat_id, needName);
+  //     return;
+  //   }
+  // }
+  
+
+  // Новый код без вызова биографии
+  if(user.currentAction == UserActions.input_name){
+    if(text == "/start") user.setUserName(name);
+    else {
+      text = text
+         .replace(/&/g, "")
+         .replace(/</g, "")
+         .replace(/>/g, "")
+         .replace(/"/g, "");
+      if(!text) text = name;
+      user.setUserName(text);
+    }
+    //user.setUserCurrentAction(UserActions.input_bio);
+    user.setUserCurrentAction(UserActions.without_action);
+    user.setUserBio("-");
+    user.setRating(1000);
+    //botSendMessage(chat_id, needBio);
+    if(user.role != "участник"){
+      user.setUserRole("участник");
+    }
+    botSendMessage(chat_id, regDone);
+    sendPlayerCard();
+    return;
+  }
+
+
+
+  /* Старый код обязательного вызова биографии
+  if(user.currentAction == UserActions.input_name){
+    if(text == "/start") user.setUserName(name);
+    else {
+      text = text
+         .replace(/&/g, "")
+         .replace(/</g, "")
+         .replace(/>/g, "")
+         .replace(/"/g, "");
+      if(!text) text = name;
+      user.setUserName(text);
+    }
+    user.setUserCurrentAction(UserActions.input_bio);
+    user.setRating(1000);
+    botSendMessage(chat_id, needBio);
+    return;
+  }
+
+*/
+
+
   // start
   if (text.startsWith("/start ")) { 
     let payload = text.split(" ")[1];
